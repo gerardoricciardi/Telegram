cf7227b99b91c041dd457c4a4810d7ba7d0fe36d
Pagliaro Antonio <pagliaroantonio@10-25-48-28.0200.san.it.iosda.org>
2017-09-22 15:01:09 +0200

    tableCell

---
+++ Telegram/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Telegram
+//
+//  Created by Pagliaro Antonio on 22/09/2017.
+//  Copyright © 2017 Ricciardi Gerardo. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

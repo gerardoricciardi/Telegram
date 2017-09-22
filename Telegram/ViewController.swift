e6ed706a878128d71bf8e0c0040d2a0a7f7f672b
Iammarino Martina <iammarinomartina@san-23.local>
2017-09-22 15:06:50 +0200

    ciao

--- Telegram/ViewController.swift
+++ Telegram/ViewController.swift
@@ -22,9 +22,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func delete(_ sender: Any?) {
-        
-    }
+   
 
 
 }

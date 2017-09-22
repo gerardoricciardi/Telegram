501e843a293f69761d3c2db13f37c592378e93dd
Ricciardi Gerardo <ricciardigerardo@10-25-48-40.0200.san.it.iosda.org>
2017-09-22 15:11:18 +0200

    non so che cosa

--- Telegram/ViewController.swift
+++ Telegram/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var prova = "Daniela mi leggi"
+    var prova = "prova1"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()

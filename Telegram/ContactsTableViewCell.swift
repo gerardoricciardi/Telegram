eea5be7bf5b9e9ba459eadebfe36326b16fd3ab3
Iammarino Martina <iammarinomartina@san-23.local>
2017-09-22 18:49:30 +0200

    contacts

---
+++ Telegram/ContactsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactsTableViewCell.swift
+//  Telegram
+//
+//  Created by Iammarino Martina on 22/09/17.
+//  Copyright © 2017 Ricciardi Gerardo. All rights reserved.
+//
+
+import UIKit
+
+class ContactsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

c2f07c9764fda169d53025ac11d56bbf47518e00
Liguori Giorgia <liguorigiorgia@10-25-48-13.0200.san.it.iosda.org>
2017-09-22 15:11:11 +0200

    sto cazzi

--- Telegram/ViewController.swift
+++ Telegram/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     var prova = "Daniela mi leggi"
+    var p2 = "ciao"
     
     override func viewDidLoad() {
         super.viewDidLoad()

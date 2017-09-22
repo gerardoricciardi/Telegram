3f0486cbc1b775713e3bf10cddf39c8846dd3db6
Pagliaro Antonio <pagliaroantonio@san-17.local>
2017-09-22 19:32:04 +0200

    Creazione file TableViewControllerChat e TableViewCellChat

---
+++ Telegram/TableViewCellChat.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCellChat.swift
+//  Telegram
+//
+//  Created by Pagliaro Antonio on 22/09/2017.
+//  Copyright © 2017 Ricciardi Gerardo. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellChat: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

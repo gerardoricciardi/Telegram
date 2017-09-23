aa6d917b202efdca85dbea8cef833212c25bb65f
Ricciardi Gerardo <ricciardigerardo@san-06.local>
2017-09-23 19:29:03 +0200

    aggiunta seconda dettagli impostazioni

--- Telegram/DetailsTableViewController.swift
+++ Telegram/DetailsTableViewController.swift
@@ -36,7 +36,7 @@ class DetailsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return riga.count
     }
 
     
